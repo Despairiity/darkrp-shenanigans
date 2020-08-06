0097b51240df4deadca7a6ff72f5725b45de32c6
Despairiity <69322818+Despairiity@users.noreply.github.com>
2020-08-06 19:48:35 -0400

    Create ammo.lua

---
+++ ammo.lua
@@ -0,0 +1,36 @@
+DarkRP.createAmmoType("357", {
+	name = "M9K 357 ammo",
+	model = "models/Items/357ammobox.mdl",
+	price = 60,
+	amountGiven = 20
+})
+DarkRP.createAmmoType("ar2", {
+	name = "M9K Rifle ammo",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 60,
+	amountGiven = 30
+})
+DarkRP.createAmmoType("buckshot", {
+	name = "M9K Shotgun ammo",
+	model = "models/Items/BoxBuckshot.mdl",
+	price = 40,
+	amountGiven = 8
+})
+DarkRP.createAmmoType("pistol", {
+	name = "M9K Pistol ammo",
+	model = "models/Items/BoxSRounds.mdl",
+	price = 20,
+	amountGiven = 20
+})
+DarkRP.createAmmoType("smg1", {
+	name = "M9K SMG ammo",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 40,
+	amountGiven = 60
+})
+DarkRP.createAmmoType("SniperPenetratedRound", {
+	name = "M9K Sniper ammo",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 80,
+	amountGiven = 6
+})
