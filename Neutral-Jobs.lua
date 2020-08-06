526ed3dd7368062226fbeec420c9338c56d54166
Despairiity <69322818+Despairiity@users.noreply.github.com>
2020-08-06 17:29:22 -0400

    Update Neutral-Jobs.lua

--- Neutral-Jobs.lua
+++ Neutral-Jobs.lua
@@ -1,1 +1,22 @@
-# Nothing, yet .  .  .
+TEAM_HOBO = DarkRP.createJob("Hobo", {
+    color = Color(161, 114, 73, 255),
+    model = {"models/player/corpse1.mdl"},
+    description = [[You're a Hobo, build yourself a space to beg for money. No, this doesn't mean you can build giant fucking structures in the middle of the road or block anything.
+
+Can Base = Only on the street or alleys that don't block doors.
+Can Raid = No
+Can Mug = No
+Can Counter = No
+Can Assist = No]],
+    weapons = {},
+    command = "hobo",
+    max = 3,
+    salary = 0,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = true,
+    category = "Citizens"
+})
+
+----------------------------------------------------------------------------------------------------------------------
