00f36ebf91cf56c6e67c17614c793730414fd834
Despairiity <69322818+Despairiity@users.noreply.github.com>
2020-08-06 19:35:22 -0400

    Update Government-Jobs.lua

--- Government-Jobs.lua
+++ Government-Jobs.lua
@@ -1,1 +1,25 @@
-# Nothing, yet .  .  .
+TEAM_MEDIC = DarkRP.createJob("Medic", {
+    color = Color(23, 204, 70, 255),
+    model = {"models/player/Group03m/male_07.mdl"},
+    description = [[You're a Medic, keep your Government buds alive.
+
+Can Base = PD.
+Can Raid = No
+Can Mug = No
+Can Counter = No
+Can Assist =  With other officers.]],
+    weapons = {"med_kit"},
+    command = "medic",
+    max = 3,
+    salary = 750,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = true,
+    category = "Government",
+    PlayerSpawn = function(ply)
+        ply:SetArmor(75)
+    end
+})
+
+----------------------------------------------------------------------------------------------------------------------
