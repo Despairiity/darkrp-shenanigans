bff51934ec98ee6d87824ea8f3b0edd23344607b
Despairiity <69322818+Despairiity@users.noreply.github.com>
2020-08-06 16:43:17 -0400

    Update Criminal-Jobs.lua

--- Criminal-Jobs.lua
+++ Criminal-Jobs.lua
@@ -1,1 +1,20 @@
-# Nothing, yet .  .  .
+TEAM_KIDNAPPER = DarkRP.createJob("Kidnapper", {
+    color = Color(41, 184, 28, 255),
+    model = {"models/player/gman_high.mdl"},
+    description = [[You're a Kidnapper, steal people's wallets and the limp corpses.
+
+Can Base = Yes
+Can Raid = No
+Can Mug = No
+Can Counter = No
+Can Assist = ( ? )]],
+    weapons = { // put a kidnapper swep or something here },
+    command = "kidnapper",
+    max = 4,
+    salary = 500,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = true,
+    category = "Criminals"
+})
