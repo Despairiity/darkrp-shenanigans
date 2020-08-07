156c0a5f7eaac47e6b973134eb9bc27ebdc84686
Despairiity <69322818+Despairiity@users.noreply.github.com>
2020-08-06 20:20:13 -0400

    Create Dealer-Jobs.lua

---
+++ Dealer-Jobs.lua
@@ -0,0 +1,68 @@
+TEAM_BGUN = DarkRP.createJob("Black Market Dealer", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/player/soldier_stripped.mdl"},
+    description = [[You're a Black Market Dealer, sell some illegals !
+    
+    Can Base = Only in Shops.
+    Can Raid = No
+    Can Mug = No
+    Can Counter = No
+    Can Assist = No]],
+    weapons = {},
+    command = "bdealer",
+    max = 3,
+    salary = 500,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = true,
+    category = "Dealers"
+})
+
+----------------------------------------------------------------------------------------------------------------------
+
+TEAM_GUN = DarkRP.createJob("Gun Dealer", {
+    color = Color(204, 149, 46, 255),
+    model = {"models/player/barney.mdl"},
+    description = [[You're a Gun Dealer, sell some guns and make some money !
+
+Can Base = Only in Shops.
+Can Raid = No
+Can Mug = No
+Can Counter = No
+Can Assist = No]],
+    weapons = {},
+    command = "gun",
+    max = 5,
+    salary = 500,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = true,
+    category = "Dealers"
+})
+
+----------------------------------------------------------------------------------------------------------------------
+
+TEAM_HGUN = DarkRP.createJob("Heavy Gun Dealer", {
+    color = Color(204, 149, 46, 255),
+    model = {"models/player/magnusson.mdl"},
+    description = [[You're a Heavy Gun Dealer, sell some big ass guns and make some money !
+
+Can Base = Only in Shops.
+Can Raid = No
+Can Mug = No
+Can Counter = No
+Can Assist = No]],
+    weapons = {},
+    command = "heavygun",
+    max = 4,
+    salary = 500,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = true,
+    category = "Dealers"
+})
+
+----------------------------------------------------------------------------------------------------------------------
